911da958103a538ac13a016d339a08c4bcde2d57
Maryam Khazaei <72004978+KhazaeiM@users.noreply.github.com>
2021-11-01 22:57:57 +0330

    Add Traveling Stations

--- DFS_ Algorithm.py
+++ DFS_ Algorithm.py
@@ -50,6 +50,24 @@ def path_list(path):
     return path_lis
 
 
+def travel_path(path, begin):
+    travel_lis = []
+    station = path.pop(-1)
+    travel_lis.append(station)
+    while station[1] != begin:
+        # Find links of station
+        link = links(data, station)
+        for lk in link:
+            # IF links are in the end of path THEN append to travel_path
+            if lk[1] == path[-1][1]:
+                travel_lis.append(lk)
+        # Check last station of path
+        path.pop(-1)
+        station = travel_lis[-1]
+    travel_lis.reverse()
+    return travel_lis
+
+
 data = pd.read_csv('metro_stations_sparse_matrix_distoriented.csv')
 # Preparing Data Frame
 data = data.fillna(0)
@@ -57,12 +75,14 @@ data = data.iloc[:, 1:]
 label = data.columns
 data.index = label
 
-begin = input("Please enter your origin: ")
+begin = input("Please enter your beginning: ")
 end = input("Please enter your destination: ")
 path = dfs(data, begin, end)
 show_path = path_list(path)
 cost = path_cost(path)
-print(show_path)
-print(cost)
-
-
+travel_path = travel_path(path, begin)
+cost_travel_path = path_cost(travel_path)
+print("DFS:", show_path)
+print("DFS COST:", cost)
+print("TRAVEL PATH:", path_list(travel_path))
+print("TRAVEL COST:", cost_travel_path)
